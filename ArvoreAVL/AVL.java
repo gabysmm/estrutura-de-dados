0c979f65c0212f7b59dd43ca35f80a44c4e2c62e
Gabrielly Magalhães <157931491+gabysmm@users.noreply.github.com>
2026-04-08 11:45:06 +0000

    feat: adicionar rotacao esquerda simples

--- ArvoreAVL/AVL.java
+++ ArvoreAVL/AVL.java
@@ -1,3 +1,28 @@
 package ArvoreAVL;
-import ArvoreBinaria.arvoreABP;
+import ArvoreBinaria.ABP;
 import ArvoreBinaria.NodeABP;
+
+public class AVL extends ABP {
+    public AVL() {
+        super(); //constructor da abp
+    }
+
+    private NodeAVL rotacaoEsquerda(NodeAVL A) {
+        NodeAVL B = (NodeAVL) A.getDir();
+
+        A.setDir(B.getEsq());
+        B.setEsq(A);
+
+        //clculo fb
+        int fbA = A.getFB();
+        int fbB = B.getFB();
+
+        int fbB_novo = fbB + 1 - Math.min(fbA, 0);
+        int fbA_novo = fbA + 1 + Math.max(fbB_noxo, 0);
+
+        B.setFB(fbB_novo);
+        A.setFB(fbA_novo);
+
+        return B;
+    }
+}
